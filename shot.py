3f7b90380ba1e893d18e54dd0fdf1cd3f0da15ed
Philipp <Philipp@mail.de>
2025-03-18 22:33:52 +0100

    add shot

--- shot.py
+++ shot.py
@@ -1,17 +1,18 @@
 import pygame
 from circleshape import CircleShape
+from constants import SHOT_RADIUS
 
-class Asteroid(CircleShape):
-    
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
-    
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
     def draw(self, screen):
         pygame.draw.circle(screen, 
                            center=self.position, 
                            radius=self.radius, 
                            width=2,
-                           color=pygame.Color("gray30"))
+                           color=pygame.Color("red"))
     
     def update(self, dt):
         self.position += self.velocity * dt
